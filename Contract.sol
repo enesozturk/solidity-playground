61c52d39863f431b2be3ff296f94e56e25a755ab
Enes Ozturk <enesozturk.d@gmail.com>
2021-09-28 13:21:49 +0300

    lessin 1 chapter 13: events

--- Contract.sol
+++ Contract.sol
@@ -4,6 +4,7 @@ contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10**dnaDigits;
 
+    event NewZombie(uint zombieId, string name, uint dna);
 
     struct Zombie {
         string name;
@@ -13,7 +14,8 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function _createZombie (string memory _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna (string memory _str) private view returns (uint) {
