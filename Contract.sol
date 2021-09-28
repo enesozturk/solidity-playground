f3568cebd0039582a4e5059ee8ce8287b8f7ea7e
Enes Ozturk <enesozturk.d@gmail.com>
2021-09-28 13:02:12 +0300

    lesson 1 chapter 11: keccak256 and typecasting

--- Contract.sol
+++ Contract.sol
@@ -17,5 +17,7 @@ contract ZombieFactory {
     }
 
     function _generateRandomDna (string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
     }
 }
