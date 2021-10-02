6e7f69f6643c914a1fb825580d5e9d17c77ce9de
Enes Ozturk <enesozturk.d@gmail.com>
2021-10-02 23:08:05 +0300

    lesson 2 chapter 7: storage vs memory

--- lesson-2/zombiefeeding.sol
+++ lesson-2/zombiefeeding.sol
@@ -4,4 +4,10 @@ import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+    }
+
+
 }
