8c7771781c4c3fcadd056f3e1265f64d42528def
Enes Ozturk <enesozturk.d@gmail.com>
2021-10-18 00:18:13 +0300

    refactor: lesson 3 chapter 1: immutability of contracts

--- lesson-3: advanced solidity concepts/zombiefeeding.sol
+++ lesson-3: advanced solidity concepts/zombiefeeding.sol
@@ -47,9 +47,9 @@ contract ZombieFeeding is ZombieFactory {
     }
 
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
-    uint kittyDna;
-    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    feedAndMultiply(_zombieId, kittyDna, "kitty");
+        uint kittyDna;
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
     }
 
 }
