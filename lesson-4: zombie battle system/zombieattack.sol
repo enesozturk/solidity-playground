58f3dc9fe11d19e117728eb903e97076c88ad62a
Enes Ozturk <enesozturk.d@gmail.com>
2021-10-22 01:44:46 +0300

    chapter 4 lesson 5: zombie fightin'

--- lesson-4: zombie battle system/zombieattack.sol
+++ lesson-4: zombie battle system/zombieattack.sol
@@ -4,9 +4,14 @@ import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
     int randNonce = 0;
+    uint attackVictoryProbability = 70;
 
     function randMod(uint _modulus) internal returns (uint) {
         randNonce++;
         return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
+    }
+
+    function attack(uint _zombieId, uint _targetId) external {
+        
     }
 }
