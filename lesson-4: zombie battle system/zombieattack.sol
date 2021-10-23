9e6619974c58bc8c35187aee85638d654bbb5a6e
Enes Ozturk <enesozturk.d@gmail.com>
2021-10-24 01:53:14 +0300

    chapter 4 lesson 8: back to attack

--- lesson-4: zombie battle system/zombieattack.sol
+++ lesson-4: zombie battle system/zombieattack.sol
@@ -12,6 +12,8 @@ contract ZombieAttack is ZombieHelper {
     }
 
     function attack(uint _zombieId, uint _targetId) external {
-        
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+        uint rand = randMod(100);
     }
 }
