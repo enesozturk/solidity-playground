0ecc9e9ff3b714bd82407b24edb9cb7dd21f37db
Enes Ozturk <enesozturk.d@gmail.com>
2021-10-26 02:01:47 +0300

    chapter 4 lesson 11: zombie loss

--- lesson-4: zombie battle system/zombieattack.sol
+++ lesson-4: zombie battle system/zombieattack.sol
@@ -20,6 +20,10 @@ contract ZombieAttack is ZombieHelper {
             myZombie.level++;
             enemyZombie.lossCount++;
             feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+        } else {
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+            _triggerCooldown(myZombie);
         }
     }
 }
