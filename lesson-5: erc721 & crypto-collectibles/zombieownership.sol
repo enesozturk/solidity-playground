6f0ad3adb919c790fdc7778be4ea04e4a9cd9d4e
Enes Ozturk <enesozturk.d@gmail.com>
2021-10-28 01:53:38 +0300

    lesson 5 chapter 3: balanceOf & ownerOf

--- lesson-5: erc721 & crypto-collectibles/zombieownership.sol
+++ lesson-5: erc721 & crypto-collectibles/zombieownership.sol
@@ -4,5 +4,11 @@ import "./zombieattack.sol";
 import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
-    
+    function balanceOf(address _owner) external view returns (uint256) {
+        return ownerZombieCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        return zombieToOwner[_tokenId];
+    }
 }
