21ec8fc44d4165452e3001e58717966cb8a5c5eb
Enes Ozturk <enesozturk.d@gmail.com>
2021-10-29 02:16:46 +0300

    lesson 5 chapter 6: erc721: transfer cont'd

--- lesson-5: erc721 & crypto-collectibles/zombieownership.sol
+++ lesson-5: erc721 & crypto-collectibles/zombieownership.sol
@@ -4,6 +4,9 @@ import "./zombieattack.sol";
 import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+
+    mapping (uint => address) zombieApprovals;
+    
     function balanceOf(address _owner) external view returns (uint256) {
         return ownerZombieCount[_owner];
     }
@@ -17,5 +20,10 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
         ownerZombieCount[_from]--;
         zombieToOwner[_tokenId] = _to;
         emit Transfer(_from, _to, _tokenId);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        require(zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+        _transfer(_from, _to, _tokenId);
     }
 }
