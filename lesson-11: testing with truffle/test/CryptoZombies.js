e48a97ae6eb29c1004841701b23ebf6ff56f4661
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-21 21:27:01 +0300

    lesson 11 chapter 5

--- lesson-11: testing with truffle/test/CryptoZombies.js
+++ lesson-11: testing with truffle/test/CryptoZombies.js
@@ -4,5 +4,11 @@ contract("CryptoZombies", (accounts) => {
   const [alice, bob] = accounts;
   it("should be able to create a new zombie", async () => {
     const contractInstance = await CryptoZombies.new();
+    const result = await contractInstance.createRandomZombie(zombieNames[0], {
+      from: alice,
+    });
+
+    assert.equal(result.receipt.status, true);
+    assert.equal(result.logs[0].args.name, zombieNames[0]);
   });
 });
