5ad3c38669d01ff0b0bad4984994072b3ed63a86
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-21 21:32:01 +0300

    lesson 11 chapter 7

--- lesson-11: testing with truffle/test/CryptoZombies.js
+++ lesson-11: testing with truffle/test/CryptoZombies.js
@@ -1,7 +1,18 @@
 const CryptoZombies = artifacts.require("CryptoZombies");
+const zombieNames = ["Zombie 1", "Zombie 2"];
 
 contract("CryptoZombies", (accounts) => {
   const [alice, bob] = accounts;
+  let contractInstance;
+
+  beforeEach(async () => {
+    contractInstance = await CryptoZombies.new();
+  });
+
+  afterEach(async () => {
+    await contractInstance.kill();
+  });
+
   it("should be able to create a new zombie", async () => {
     const contractInstance = await CryptoZombies.new();
     const result = await contractInstance.createRandomZombie(zombieNames[0], {
@@ -10,5 +21,12 @@ contract("CryptoZombies", (accounts) => {
 
     assert.equal(result.receipt.status, true);
     assert.equal(result.logs[0].args.name, zombieNames[0]);
+  });
+
+  it("should not allow two zombies", async () => {
+    await contractInstance.createRandomZombie(zombieNames[0], { from: alice });
+    await utils.shouldThrow(
+      contractInstance.createRandomZombie(zombieNames[1], { from: alice })
+    );
   });
 });
