3d9e1f769801c32c77f9466f9db3e4a8eee49f19
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-22 19:35:59 +0300

    lesson 11 chapter 8: context and skip tests

--- lesson-11: testing with truffle/test/CryptoZombies.js
+++ lesson-11: testing with truffle/test/CryptoZombies.js
@@ -29,4 +29,19 @@ contract("CryptoZombies", (accounts) => {
       contractInstance.createRandomZombie(zombieNames[1], { from: alice })
     );
   });
+
+  xcontext("with the single-step transfer scenario", async () => {
+    it("should transfer a zombie", async () => {
+      // TODO: Test the single-step transfer scenario.
+    });
+  });
+
+  xcontext("with the two-step transfer scenario", async () => {
+    it("should approve and then transfer a zombie when the approved address calls transferFrom", async () => {
+      // TODO: Test the two-step scenario.  The approved address calls transferFrom
+    });
+    it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
+      // TODO: Test the two-step scenario.  The owner calls transferFrom
+    });
+  });
 });
