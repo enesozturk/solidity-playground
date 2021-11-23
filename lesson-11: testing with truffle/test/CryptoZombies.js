68e7aaff475030a114671c27b996484bb901c12d
Enes Ozturk <enesozturk.d@gmail.com>
2021-11-24 01:05:08 +0300

    lesson 11 chapter 12: attacks

--- lesson-11: testing with truffle/test/CryptoZombies.js
+++ lesson-11: testing with truffle/test/CryptoZombies.js
@@ -70,4 +70,21 @@ contract("CryptoZombies", (accounts) => {
       assert.equal(newOwner, bob);
     });
   });
+
+  it("zombies should be able to attack another zombie", async () => {
+    let result;
+    result = await contractInstance.createRandomZombie(zombieNames[0], {
+      from: alice,
+    });
+    const firstZombieId = result.logs[0].args.zombieId.toNumber();
+    result = await contractInstance.createRandomZombie(zombieNames[1], {
+      from: bob,
+    });
+    const secondZombieId = result.logs[0].args.zombieId.toNumber();
+    await time.increase(time.duration.days(1));
+    await contractInstance.attack(firstZombieId, secondZombieId, {
+      from: alice,
+    });
+    assert.equal(result.receipt.status, true);
+  });
 });
